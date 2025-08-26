76c7dbec4a784225ec25269688c8a4667ab5b7c0
Einari Mäkiranta <makirantaeinari@gmail.com>
2025-08-26 06:57:56 +0300

    Removed asset importer

--- crates/nox-asset/asset_importer/bin_gen.rs
+++
@@ -1,8 +0,0 @@
-use std::path::{Path, PathBuf};
-
-use super::{LoadError, AssetType};
-
-pub trait BinGen<A: AssetType> {
-
-    fn generate(&mut self, asset_type: A, in_path: &Path) -> Result<PathBuf, LoadError>;
-}
